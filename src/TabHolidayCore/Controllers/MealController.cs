2d9c50c7a82fe0213bcbe2a8824b7ea75834f1fa
unknown <anjalirshirke@gmail.com>
2018-03-14 18:27:22 +0530

    TabMeal Edit functionality added

--- src/TabHolidayCore/Controllers/MealController.cs
+++ src/TabHolidayCore/Controllers/MealController.cs
@@ -70,5 +70,44 @@ namespace TabHolidayCore.Controllers
             return returnObject.GetResponse();
         }
 
+
+        [HttpPost]
+        [Authorize]
+        public string GetTabMeals([FromBody]TabMeal meal)
+        {
+            try
+            {
+                var TabMealQuery = _context.TabMeals.AsQueryable();
+
+            
+                if (meal.Name != string.Empty && meal.Name != null)
+                {
+                    TabMealQuery = TabMealQuery.Where(d => d.Name.Contains(meal.Name));
+                }
+
+
+                if (meal.City != string.Empty && meal.City != null)
+                {
+                    TabMealQuery = TabMealQuery.Where(d => d.City.Contains(meal.City));
+                }
+
+                if (meal.RestaurantTypeId > 0)
+                {
+                    TabMealQuery = TabMealQuery.Where(d => d.RestaurantTypeId == meal.RestaurantTypeId);
+                }
+
+
+                returnObject.isSuccess = true;
+                returnObject.ChangedData = TabMealQuery.Include(d => d.RestaurantType).ToArray();
+            }
+            catch (Exception ex)
+            {
+
+                returnObject.isSuccess = false;
+                returnObject.Message = returnObject.GetErrorMessage(ex);
+            }
+
+            return returnObject.GetResponse();
+        }
     }
 }
--- src/TabHolidayCore/wwwroot/lib/tabHolidayApp/meal/tabmealController.js
+++ src/TabHolidayCore/wwwroot/lib/tabHolidayApp/meal/tabmealController.js
@@ -1,12 +1,61 @@
 ﻿myapp.controller('MealController', ['$http', '$scope', '$location', 'myappService', '$filter', function ($http, $scope, $location, myappService, $filter) {
 
+    $scope.ViewMode = "list";
+
     $scope.Meal = {};
+    
+
+
+    $scope.GetAllTabMeals = function () {
+        $http.get('/Meal').then(
+              function (successResponse) {
+                  if (successResponse.data.isSuccess) {
+
+                      $scope.TabMeals = successResponse.data.ChangedData;
+
+                  }
+                  else {
+
+                      alert(successResponse.data.Message);
+
+                  }
+              },
+              function (errorResponse) {
+                  // handle errors here
+              });
+    };
+
+
+    $scope.GetTabMeals = function () {
+
+        $http.post('/Meal/GetTabMeals', $scope.Meal).then(
+             function (successResponse) {
+                 if (successResponse.data.isSuccess) {
+                     $scope.TabMeals = successResponse.data.ChangedData;
+                    
+                 }
+                 else {
+
+                     alert(successResponse.data.Message);
+
+                 }
+             },
+             function (errorResponse) {
+                 // handle errors here
+             });
+
+    };
+
 
     $scope.LoadData = function () {
         $scope.FoodTypes = myappService.GetFoodTypes();
         $scope.RestaurantTypes = myappService.GetRestaurantTypes();
-
+        $scope.GetAllTabMeals();
+        $scope.GetTabMeals();
+       
+        
     };
+
     $scope.LoadData();
 
     $scope.$on("CollectedMasters", function () {
@@ -15,8 +64,13 @@
     });
 
     $scope.Save = function () {
+     
+            $scope.Meal.OpeningHour = Meal.OpeningTime.getHours();
+            $scope.Meal.OpeningMinute = Meal.OpeningTime.getMinutes();
 
-
+            $scope.Meal.ClosingHour = Meal.ClosingTime.getHours();
+            $scope.Meal.ClosingMinute = Meal.ClosingTime.getMinutes();
+        
         $http.post('/Meal/Add', $scope.Meal).then(
             function (successResponse) {
                 if (successResponse.data.isSuccess) {
@@ -36,5 +90,27 @@
               });
 
     };
+
+
+    $scope.Edit = function (TabMeal) {
+
+        $scope.IsEditMode = true;
+        $scope.ViewMode = "new";
+        $scope.Meal = TabMeal;
+        $scope.Meal.FoodTypeId = $scope.Meal.FoodTypeId.toString();
+        $scope.Meal.RestaurantTypeId = $scope.Meal.RestaurantTypeId.toString();
+    
+
+    };
+
+    //$scope.Reset = function () {
+    //    $scope.Meal = {};
+    //    $scope.IsEditMode = false;
+    //};
+
+    $scope.InitializeTabMealSearchObject = function () {
+        $scope.Meal = {};
+    };
+    
   
 }]);
