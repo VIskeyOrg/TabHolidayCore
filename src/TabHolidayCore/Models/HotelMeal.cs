399f3cb97bbf057e64073b20ac9df01bbc7510d2
vishaloutlook710 <36813217+vishaloutlook710@users.noreply.github.com>
2018-03-17 02:23:59 +0530

    Update HotelMeal.cs

--- src/TabHolidayCore/Models/HotelMeal.cs
+++ src/TabHolidayCore/Models/HotelMeal.cs
@@ -4,22 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/origin/anjali
 namespace TabHolidayCore.Models
 {
     public class HotelMeal
     {
         [Key]
-<<<<<<< HEAD
         public Int16 HotelMealId { get; set; }
         public decimal Price { get; set; }
-=======
-        public Int64 HotelMealId { get; set; }
-        public Decimal Price { get; set; }
->>>>>>> refs/remotes/origin/anjali
+
 
         public Int16 MealId { get; set; }
         public Meal Meal { get; set; }
